eebe4cc3ca53e9cb6e990c3a7073ac0a0b79f3ee
robertbutacu <robertbutacu@gmail.com>
2018-08-28 13:05:53 +0100

    designing the board states

--- src/main/scala/elements/boards/BoardState.scala
+++ src/main/scala/elements/boards/BoardState.scala
@@ -1,11 +1,14 @@
 package elements.boards
 
+import actions.PiecePosition
 import players.Player
 
 trait BoardState {
   def player1: Player
   def player2: Player
   def playerTurn: Int
+  def player1KingPosition: PiecePosition
+  def player2KingPosition: PiecePosition
 
   def next: BoardState
 }
---
+++ src/main/scala/elements/boards/StalemateState.scala
@@ -0,0 +1,5 @@
+package elements.boards
+
+class StalemateState {
+
+}
