eb7beb68d8d60456936fc3bd4153d6dc8bbb40fe
robertbutacu <robertbutacu@gmail.com>
2018-08-29 12:11:09 +0100

    scaffolding of check state next()

--- src/main/scala/elements/boards/states/CheckState.scala
+++ src/main/scala/elements/boards/states/CheckState.scala
@@ -2,14 +2,26 @@ package elements.boards.states
 
 import elements.boards.BoardState
 import elements.boards.information.{KingsPositions, Players}
-import elements.pieces.Piece
+import elements.pieces.{King, Piece}
 
 case class CheckState() extends BoardState {
   override def players: Players = ???
 
   override def kingsPositions: KingsPositions = ???
 
-  override def next: Option[BoardState] = ???
+  override def next: Option[BoardState] = {
+    val nextMove = players.getPlayerTurn.askForMove
+
+    val pieceToBeMoved = pieces(nextMove.from.X)(nextMove.from.Y)
+
+    if (pieceToBeMoved.owner.contains(players.getPlayerTurn) && (pieceToBeMoved match {
+      case _: King => true;
+      case _ => false
+    })) {
+      if (isPlayerKingUncovered) None
+      else None
+    } else None
+  }
 
   override def pieces: List[List[Piece]] = ???
 
