1d79aabb968e50b072b9da9e909088c690454339
robertbutacu <robertbutacu@gmail.com>
2018-08-29 12:05:16 +0100

    architectural refactoring

--- src/main/scala/elements/pieces/Piece.scala
+++ src/main/scala/elements/pieces/Piece.scala
@@ -5,5 +5,60 @@ import players.Player
 
 trait Piece {
   def owner: Option[Player]
+
   def piecePosition: PiecePosition
+}
+
+case class Pawn(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def owner: Option[Player] = Some(player)
+
+  override def toString: String =
+    if (player.index == 1) "P1"
+    else "P2"
+}
+
+case class Knight(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def toString: String =
+    if (player.index == 1) "Kn1"
+    else "Kn2"
+
+  override def owner: Option[Player] = Some(player)
+}
+
+case class King(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def toString: String =
+    if (player.index == 1) "Ki1"
+    else "Ki2"
+
+  override def owner: Option[Player] = Some(player)
+}
+
+case class Queen(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def toString: String =
+    if (player.index == 1) "Q1"
+    else "Q2"
+
+  override def owner: Option[Player] = Some(player)
+}
+
+case class Rook(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def toString: String =
+    if (player.index == 1) "R1"
+    else "R2"
+
+  override def owner: Option[Player] = Some(player)
+}
+
+case class Empty(piecePosition: PiecePosition) extends Piece {
+  override def toString: String = "  "
+
+  override def owner: Option[Player] = None
+}
+
+case class Bishop(player: Player, piecePosition: PiecePosition) extends Piece {
+  override def toString: String =
+    if (player.index == 1) "B1"
+    else "B2"
+
+  override def owner: Option[Player] = Some(player)
 }
