92fbb6840e2c3ec398dd6a3d71bc99b0db0f6c9f
robertbutacu <robertbutacu@gmail.com>
2018-08-30 17:09:09 +0100

    generalising boards

--- src/test/scala/elements/pieces/moves/Board.scala
+++ src/test/scala/elements/pieces/moves/Board.scala
@@ -5,9 +5,14 @@ import elements.boards.states.NoSpecialState
 import elements.pieces._
 import players.AIPlayer
 
+import scala.language.postfixOps
+
 trait Board {
   val genericPlayer = AIPlayer("XXX", 1)
   val genericPosition = PiecePosition(4, 4)
+
+  lazy val emptyRow: List[Piece] = (0 to 7) map (i => Empty(PiecePosition(i, 0))) toList
+  lazy val pawnRow: List[Piece] = (0 to 7) map { i => Pawn(genericPlayer, PiecePosition(i, 0)) } toList
 
   lazy val rook = Rook(genericPlayer, genericPosition)
   lazy val queen = Queen(genericPlayer, genericPosition)
@@ -17,5 +22,17 @@ trait Board {
   lazy val knight = Knight(genericPlayer, genericPosition)
 
   lazy val board = NoSpecialState(List.empty)
+
+  def mapToRow(rowIndex: Int) = emptyRow.map(p => Empty(PiecePosition(p.position.X, rowIndex)))
+
+  def addPieceToMiddleRow(p: Piece, row: List[Piece]): List[Piece] = ???
+
+  lazy val rookBoard = List(0 to 7 map mapToRow)
+  lazy val queenBoard = List(0 to 7 map mapToRow)
+  lazy val kingBoard = List(0 to 7 map mapToRow)
+  lazy val bishopBoard = List(0 to 7 map mapToRow)
+  lazy val pawnBoard = List(0 to 7 map mapToRow)
+  lazy val knightBoard = List(0 to 7 map mapToRow)
+
 
 }
