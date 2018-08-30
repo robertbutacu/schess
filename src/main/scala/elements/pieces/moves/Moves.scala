e994d9fead41acebd8af6941393d8043e91df10e
robertbutacu <robertbutacu@gmail.com>
2018-08-30 23:55:38 +0100

    indentation

--- src/main/scala/elements/pieces/moves/Moves.scala
+++ src/main/scala/elements/pieces/moves/Moves.scala
@@ -25,14 +25,14 @@ object Moves {
     val yAxis = to.Y - from.Y
 
     (xAxis, yAxis) match {
-      case (left, down) if down < 0 && left < 0            => diagonalLeftDown
-      case (right, down) if down < 0 && right > 0          => diagonalRightDown
-      case (left, up) if up > 0 && left < 0                => diagonalLeftUp
-      case (right, up) if up > 0 && right > 0              => diagonalRightUp
-      case (straight, up) if straight == 0 && up > 0       => straightUp
-      case (straight, down) if straight == 0 && down < 0   => straightDown
+      case (left, down)      if down < 0 && left < 0       => diagonalLeftDown
+      case (right, down)     if down < 0 && right > 0      => diagonalRightDown
+      case (left, up)        if up > 0 && left < 0         => diagonalLeftUp
+      case (right, up)       if up > 0 && right > 0        => diagonalRightUp
+      case (straight, up)    if straight == 0 && up > 0    => straightUp
+      case (straight, down)  if straight == 0 && down < 0  => straightDown
       case (right, straight) if right > 0 && straight == 0 => straightRight
-      case (left, straight) if left < 0 && straight == 0   => straightLeft
+      case (left, straight)  if left < 0 && straight == 0  => straightLeft
     }
   }
 }
