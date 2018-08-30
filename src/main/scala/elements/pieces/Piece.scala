10126d7ffa7bbea324f52c366ae07cf65847215c
robertbutacu <robertbutacu@gmail.com>
2018-08-30 09:24:03 +0100

    renaming

--- src/main/scala/elements/pieces/Piece.scala
+++ src/main/scala/elements/pieces/Piece.scala
@@ -6,10 +6,10 @@ import players.Player
 trait Piece {
   def owner: Option[Player]
 
-  def piecePosition: PiecePosition
+  def position: PiecePosition
 }
 
-case class Pawn(player: Player, piecePosition: PiecePosition) extends Piece {
+case class Pawn(player: Player, position: PiecePosition) extends Piece {
   override def owner: Option[Player] = Some(player)
 
   override def toString: String =
@@ -17,7 +17,7 @@ case class Pawn(player: Player, piecePosition: PiecePosition) extends Piece {
     else "P2"
 }
 
-case class Knight(player: Player, piecePosition: PiecePosition) extends Piece {
+case class Knight(player: Player, position: PiecePosition) extends Piece {
   override def toString: String =
     if (player.index == 1) "Kn1"
     else "Kn2"
@@ -25,7 +25,7 @@ case class Knight(player: Player, piecePosition: PiecePosition) extends Piece {
   override def owner: Option[Player] = Some(player)
 }
 
-case class King(player: Player, piecePosition: PiecePosition) extends Piece {
+case class King(player: Player, position: PiecePosition) extends Piece {
   override def toString: String =
     if (player.index == 1) "Ki1"
     else "Ki2"
@@ -33,7 +33,7 @@ case class King(player: Player, piecePosition: PiecePosition) extends Piece {
   override def owner: Option[Player] = Some(player)
 }
 
-case class Queen(player: Player, piecePosition: PiecePosition) extends Piece {
+case class Queen(player: Player, position: PiecePosition) extends Piece {
   override def toString: String =
     if (player.index == 1) "Q1"
     else "Q2"
@@ -41,7 +41,7 @@ case class Queen(player: Player, piecePosition: PiecePosition) extends Piece {
   override def owner: Option[Player] = Some(player)
 }
 
-case class Rook(player: Player, piecePosition: PiecePosition) extends Piece {
+case class Rook(player: Player, position: PiecePosition) extends Piece {
   override def toString: String =
     if (player.index == 1) "R1"
     else "R2"
@@ -49,13 +49,13 @@ case class Rook(player: Player, piecePosition: PiecePosition) extends Piece {
   override def owner: Option[Player] = Some(player)
 }
 
-case class Empty(piecePosition: PiecePosition) extends Piece {
+case class Empty(position: PiecePosition) extends Piece {
   override def toString: String = "  "
 
   override def owner: Option[Player] = None
 }
 
-case class Bishop(player: Player, piecePosition: PiecePosition) extends Piece {
+case class Bishop(player: Player, position: PiecePosition) extends Piece {
   override def toString: String =
     if (player.index == 1) "B1"
     else "B2"
--- src/main/scala/elements/pieces/moves/MoveValidator.scala
+++ src/main/scala/elements/pieces/moves/MoveValidator.scala
@@ -12,18 +12,18 @@ object MoveValidator {
 
   implicit class BishopValidator(board: BoardState) extends MoveValidator[Bishop] {
     override def isValidMove(piece: Bishop, to: PiecePosition): Boolean =
-      Board.isDiagonalMove(piece.piecePosition, to)
+      Board.isDiagonalMove(piece.position, to)
   }
 
   implicit class RookValidator(board: BoardState) extends MoveValidator[Rook] {
     override def isValidMove(piece: Rook, to: PiecePosition): Boolean =
-      Board.isStraightMove(piece.piecePosition, to)
+      Board.isStraightMove(piece.position, to)
   }
 
   implicit class KingValidator(board: BoardState) extends MoveValidator[King] {
     override def isValidMove(piece: King, to: PiecePosition): Boolean = {
-      val horizontalDif = to.X - piece.piecePosition.X
-      val verticalDif = to.Y - piece.piecePosition.Y
+      val horizontalDif = to.X - piece.position.X
+      val verticalDif = to.Y - piece.position.Y
 
       isAmongAllMoves(verticalDif, horizontalDif, -1, 1, -1, 1)
     }
@@ -31,13 +31,13 @@ object MoveValidator {
 
   implicit class QueenValidator(board: BoardState) extends MoveValidator[Queen] {
     override def isValidMove(piece: Queen, to: PiecePosition): Boolean =
-      Board.isStraightMove(piece.piecePosition, to) || Board.isDiagonalMove(piece.piecePosition, to)
+      Board.isStraightMove(piece.position, to) || Board.isDiagonalMove(piece.position, to)
   }
 
   implicit class KnightValidator(board: BoardState) extends MoveValidator[Knight] {
     override def isValidMove(piece: Knight, to: PiecePosition): Boolean = {
-      val verticalDif = to.X - piece.piecePosition.X
-      val horizontalDif = to.Y - piece.piecePosition.Y
+      val verticalDif = to.X - piece.position.X
+      val horizontalDif = to.Y - piece.position.Y
 
       isAmongAllMoves(verticalDif, horizontalDif, -2, +2, -1, +1)
     }
@@ -45,8 +45,8 @@ object MoveValidator {
 
   implicit class PawnValidator(board: BoardState) extends MoveValidator[Pawn] {
     override def isValidMove(piece: Pawn,to: PiecePosition): Boolean = {
-      val verticalMove = to.X - piece.piecePosition.X
-      val horizontalMove = to.Y - piece.piecePosition.Y
+      val verticalMove = to.X - piece.position.X
+      val horizontalMove = to.Y - piece.position.Y
 
       verticalMove == 1 && (horizontalMove >= -1 && horizontalMove <= 1)
     }
