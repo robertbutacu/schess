ca6a2a85d1220d204b527108152c0e9eb3f2e0be
robertbutacu <robertbutacu@gmail.com>
2018-08-31 10:04:36 +0100

    askFormove humanplayer

--- src/main/scala/players/HumanPlayer.scala
+++ src/main/scala/players/HumanPlayer.scala
@@ -1,15 +1,25 @@
 package players
 
-import actions.Move
+import actions.{Move, PiecePosition}
+
 import scala.io.StdIn.readLine
+import scala.util.{Failure, Success, Try}
 
 case class HumanPlayer(name: String, index: Int) extends Player {
   override def askForMove: Move = {
-    def getValidMove(): Move = {
-      val from = readLine("Choose the piece you want to move: \n\n")
+    def getValidMove(msg: String): Move = {
+      def getMove(msg: String) =
+        Try {
+          val from = readLine(msg)
+          PiecePosition(from)
+        }
 
-      ???
+      getMove(msg) match {
+        case Success(piece) => ???
+        case Failure(_) => getValidMove("Invalid move. Please choose again: \n")
+      }
     }
-    ???
+
+    getValidMove("Choose the piece you want to move: \n")
   }
 }
