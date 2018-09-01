cc187e7b324446804f134305da83bbe9be717210
robertbutacu <robertbutacu@gmail.com>
2018-09-01 16:47:41 +0100

    updating king position

--- src/main/scala/elements/boards/information/KingsPositions.scala
+++ src/main/scala/elements/boards/information/KingsPositions.scala
@@ -1,5 +1,14 @@
 package elements.boards.information
 
 import actions.PiecePosition
+import elements.pieces.King
+import players.{PlayerOne, PlayerTwo}
 
-case class KingsPositions(firstKingPosition: PiecePosition, secondKingPosition: PiecePosition)
+case class KingsPositions(firstKingPosition: PiecePosition, secondKingPosition: PiecePosition) {
+  def apply(kingPosition: King) = {
+    kingPosition.player.index match {
+      case PlayerOne => this.copy(firstKingPosition = kingPosition.position)
+      case PlayerTwo => this.copy(secondKingPosition = kingPosition.position)
+    }
+  }
+}
