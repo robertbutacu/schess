bc31d230bc973abe29ede1eb851adafd5bd1321a
robertbutacu <robertbutacu@gmail.com>
2018-09-04 10:10:48 +0100

    [move validator ] somehow works, still have to find an abstraction for a Piece validator

--- src/main/scala/elements/boards/BoardUtils.scala
+++ src/main/scala/elements/boards/BoardUtils.scala
@@ -9,10 +9,10 @@ object BoardUtils {
   val letterMapping = Map('A' -> 0, 'B' -> 1, 'C' -> 2, 'D' -> 3, 'E' -> 4, 'F' -> 5, 'G' -> 6, 'H' -> 7)
   val numberMapping = Map(0 -> 'A', 1 -> 'B', 2 -> 'C', 3 -> 'D', 4 -> 'E', 5 -> 'F', 6 -> 'G', 7 -> 'H')
 
-  def isKingInCheckState[P <: Piece](board: List[List[P]], playerTurn: PlayerIndex, kingsPositions: KingsPositions): Boolean = {
+  def isKingInCheckState(board: BoardState, playerTurn: PlayerIndex, kingsPositions: KingsPositions): Boolean = {
     def filterOppositePlayerPieces(): List[Piece] =
       for {
-        row   <- board
+        row   <- board.pieces
         piece <- row
         owner <- piece.owner
         if owner.index == playerTurn.otherPlayerTurn
@@ -22,21 +22,14 @@ object BoardUtils {
 
     val possibleDangers = filterOppositePlayerPieces()
 
-    possibleDangers.exists { p => board.isValidMove(p, playerKing)
-      /*case r: Rook => board.isValidMove(r, playerKing)
-      case k: King => board.isValidMove(k, playerKing)
-      case q: Queen => board.isValidMove(q, playerKing)
-      case kn: Knight => board.isValidMove(kn, playerKing)
-      case p: Pawn => board.isValidMove(p, playerKing)
-      case b: Bishop => board.isValidMove(b, playerKing)*/
-    }
+    possibleDangers exists { board.isValidMove(_, playerKing) }
   }
 
   def nextPlayerIndex(playerTurn: Int): Int = 3 - playerTurn
 
   private def isEmptyPosition(board: BoardState, X: Int, Y: Int) = board.pieces(Y)(X) match {
     case _: EmptyPosition => true
-    case _        => false
+    case _                => false
   }
 
 }
