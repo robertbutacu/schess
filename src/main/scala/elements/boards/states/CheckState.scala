61c26efb962cd4802d4e0e707f6b6e8bfa8365de
robertbutacu <robertbutacu@gmail.com>
2018-09-10 14:41:13 +0100

    [game flow] check state next() implementation

--- src/main/scala/elements/boards/states/CheckState.scala
+++ src/main/scala/elements/boards/states/CheckState.scala
@@ -1,7 +1,8 @@
 package elements.boards.states
 
+import actions.execute.MoveCategorisation
 import elements.boards.BoardState
-import elements.boards.information.{KingsPositions, Players}
+import elements.boards.information.Players
 import elements.pieces.{King, Piece}
 
 case class CheckState(pieces: List[List[Piece]],
@@ -11,12 +12,20 @@ case class CheckState(pieces: List[List[Piece]],
 
     val pieceToBeMoved = pieces(nextMove.from.X)(nextMove.from.Y)
 
-    if (pieceToBeMoved.owner.contains(players.getPlayerTurn) && (pieceToBeMoved match {
+    def isValidMove: Boolean = pieceToBeMoved.owner.contains(players.getPlayerTurn) && (pieceToBeMoved match {
       case _: King => true
       case _       => false
-    })) {
-      if (wouldPlayerKingBeInCheck(nextMove)) None
-      else None
-    } else None
+    })
+
+    if (isValidMove) {
+      if (wouldPlayerKingBeInCheck(nextMove)) {
+        println("\n The king would still be in check! Please choose another one!")
+        this.next
+      }
+      else Some(MoveCategorisation.categorise(this, nextMove.from, nextMove.to))
+    } else {
+      println("Please choose one of your pieces - keep in mind that you are in check")
+      this.next
+    }
   }
 }
