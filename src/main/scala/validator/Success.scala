a4bc31f3818b40ff4a6936d9d8663f536ffa11e1
robertbutacu <robertbutacu@gmail.com>
2018-09-26 12:02:52 +0100

    [feature] Success will create the updated board

--- src/main/scala/validator/Success.scala
+++ src/main/scala/validator/Success.scala
@@ -1,7 +1,23 @@
 package validator
 
+import actions.Position
+import actions.execute.{EnPassantMove, KingCastleMove, NormalMove}
 import elements.boards.states.BoardState
+import elements.pieces.{King, Pawn}
+import actions.validators.board.BoardQueries.BoardQueriesImplicit
 
 case class Success(message: Option[String] = None, board: BoardState) extends Validator {
   override def next: BoardState = ???
+
+  private   def categorise(board: BoardState, from: Position, to: Position): BoardState = {
+    board.getPiece(from.X,from.Y) match {
+      case king: King =>
+        if (board.isCastling(king, to)) KingCastleMove(board, from, to).go()
+        else NormalMove(board, from, to).go()
+      case pawn: Pawn =>
+        if (board.isEnPassantMove(pawn, to)) EnPassantMove(board, from, to).go()
+        else NormalMove(board, from, to).go()
+      case _          => NormalMove(board, from, to).go()
+    }
+  }
 }
