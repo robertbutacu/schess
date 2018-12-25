6270a82d1842a10f18dac89d0aa52286893092a6
robertbutacu <robertbutacu@gmail.com>
2018-12-25 18:31:08 +0200

    scaffolding StateGenerator

--- src/main/scala/game/players/models/ai/layers/StateGenerator.scala
+++ src/main/scala/game/players/models/ai/layers/StateGenerator.scala
@@ -1,5 +1,11 @@
 package game.players.models.ai.layers
 
-class StateGenerator {
+import game.elements.boards.states.BoardState
+import scala.concurrent.Future
 
+case class Layer(currentState: BoardState, children: List[Layer])
+
+object StateGenerator {
+  //TODO maybe use Tree
+  def generate(state: BoardState)(implicit difficulty: Difficulty): Future[List[Layer]] = ???
 }
